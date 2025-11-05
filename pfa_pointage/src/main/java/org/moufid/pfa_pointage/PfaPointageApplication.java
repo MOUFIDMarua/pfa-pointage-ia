5d2340a72aad49776f54d83e34459797cdd742b3
Ton Nom <ton@email.com>
2025-11-05 16:17:28 +0100

    Initial Spring Boot structure

---
+++ pfa_pointage/src/main/java/org/moufid/pfa_pointage/PfaPointageApplication.java
@@ -0,0 +1,13 @@
+package org.moufid.pfa_pointage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfaPointageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfaPointageApplication.class, args);
+	}
+
+}
---
+++ pfa_pointage/src/test/java/org/moufid/pfa_pointage/PfaPointageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moufid.pfa_pointage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfaPointageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
